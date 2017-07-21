7f20678e5feb99cdd368dce9172a894dd6781b47
AlesyaJadrovski <alesya.smirnova@hotmail.com>
2017-07-21 10:42:02 +0300

    fix

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-/*
-function openbox1(id){
-    display = document.getElementById(id).style.display;
-
-    if(display=='none'){
-        document.getElementById(id).style.display='block';
-    }else{
-        document.getElementById(id).style.display='none';
-    }
-}
-*/
 
 var list = $('.list'),
     service = list.find('.service>li'),
