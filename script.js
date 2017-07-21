e50361219f376fef32ac9b787778706499e84604
AlesyaJadrovski <alesya.smirnova@hotmail.com>
2017-07-21 17:09:54 +0300

    fix

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 
 var list = $('.list'),
+    content = $('.content'),
     services = list.find('.services>li'),
     description = list.find('.tab'),
-    serviceList = list.find('.service-list');
+    serviceList = list.find('.service-list'),
+    section = content.find('.section'),
+    sectionDescription = content.find('.section-disc');
 
 $(function () {
     services.on('click', function () {
         var currentIndex = $(this).index();
         $(description[currentIndex]).show();
         serviceList.show();
+        sectionDescription.hide();
     });
 });
 
-    services.on('click', function () {
+services.on('click', function () {
+    var currentIndex = $(this).index();
+    if (services[currentIndex] !== description[currentIndex]){
+        description.hide();
+    }
+});
+
+$(function () {
+    section.on('click', function () {
         var currentIndex = $(this).index();
-        if (services[currentIndex] !== description[currentIndex]){
-            description.hide();
-        }
-    });
+        $(sectionDescription[currentIndex]).show();
+        description.hide();
+        serviceList.hide();
+    })
+});
+
+section.on('click', function () {
+    var currentIndex = $(this).index();
+    if (section[currentIndex] !== sectionDescription[currentIndex]){
+        sectionDescription.hide();
+    }
+});
